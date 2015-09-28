9e0c6e012893ab39e0760b42fe584edd9692edb3
Karyd'ja <karydjasouza@outlook.com>
2015-09-27 22:05:07 -0300

    Fix translation in sign up page

--- spec/devise/registration_spec.rb
+++ spec/devise/registration_spec.rb
@@ -9,7 +9,7 @@ describe "User registration" do
     page.fill_in "user_password", with: 'password'
     page.fill_in "user_password_confirmation", with: 'password'
 
-    click_button I18n.t('devise.registrations.new.sign_up')
+    click_button I18n.t('devise.links.sign_up')
 
     expect(page).to have_content(I18n.t('devise.registrations.signed_up'))
   end
